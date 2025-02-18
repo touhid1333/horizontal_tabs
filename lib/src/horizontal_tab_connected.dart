7f5b40ff8dd38aef463c6c5cfafeace97d4ab4bb
Md. Touhidur Rahman <touhid1333@gmail.com>
2025-02-18 19:36:44 +0600

    Horizontal Tab Connected Started

--- lib/src/horizontal_tab_connected.dart
+++ lib/src/horizontal_tab_connected.dart
@@ -1,1 +1,15 @@
 part of '../horizontal_tabs.dart';
+
+class HorizontalTabConnected extends StatefulWidget {
+  const HorizontalTabConnected({super.key});
+
+  @override
+  State<HorizontalTabConnected> createState() => _HorizontalTabConnectedState();
+}
+
+class _HorizontalTabConnectedState extends State<HorizontalTabConnected> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
